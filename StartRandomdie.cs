67690c4793413302a64038757fbe417cdab864c7
kumarPraveen9 <gpk50626@gmail.com>
2022-12-27 23:19:00 +0530

    Printing a random die between 1 to 6

---
+++ StartRandomdie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeLadder
+{
+    internal class StartRandomdie
+    {
+        public static void start()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : "+startPosition);
+            Random random= new Random();
+                int die = random.Next(1, 7);
+                Console.WriteLine("The Player rolls the die to get a number between 1 to 6 is : "+die);
+            
+
+        }
+    }
+}
