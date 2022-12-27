e04ac6b77c01abf63dfa3695155859baec0577d2
kumarPraveen9 <gpk50626@gmail.com>
2022-12-27 23:00:09 +0530

    starting

---
+++ StartPosZero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeLadder
+{
+    internal class StartPosZero
+    {
+        public static void start()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : "+startPosition);
+        }
+    }
+}
