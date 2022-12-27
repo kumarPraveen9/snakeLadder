c05d7222698e119bc90c9a022d1b63bfb70c0fa8
kumarPraveen9 <gpk50626@gmail.com>
2022-12-27 23:16:31 +0530

    Printing a random die between 1 to 6

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snack and ladder Game\n");
-        StartPosZero.start();
+            StartRandomdie.start();
         }
     }
 }
